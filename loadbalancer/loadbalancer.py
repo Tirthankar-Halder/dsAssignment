503181d6923fcc085191375fedc6db406940d36d
Tirthankar-Halder <tirthankarhalder70@gmail.com>
2024-01-23 01:13:03 +0530

    Respawn,unwanted server remove section updated--working

--- loadbalancer/loadbalancer.py
+++ loadbalancer/loadbalancer.py
@@ -27,22 +27,45 @@ for replica in replicas:
 ######################### Difining the another thread to check server status #######################
 def replica_status(replicas):
     while True:
+
+        ####################Respwn Method 1###############################
+
         for replica in replicas:
+
             alive = None
             alive = os.system(f"ping -c 1 {replica}")
             if alive is None:
-                res=os.popen(f"sudo docker run --name {replica} --network net1 --network-alias {replica} -e 'SERVER_ID={replica}' VAR1=v1 -e VAR2=v2 -d server:latest").read()
+                res=os.popen(f"sudo docker run --name {replica} --network net1 --network-alias {replica} -e 'SERVER_ID={replica}' -d server:latest").read()
 
                 if len(res)==0:
                     print(f"Unable to start {replica}")
                 else:
                     print(f"successfully started {replica}")
-            
+
+        ####################Respwn Method 2###############################
+
+        # missingContainerOut = os.popen("sudo docker ps --format '[[ .Names ]]'").read().split("\n")
+        # missingContainerOut = missingContainerOut[:len(missingContainerOut)-1] 
+        # for replica in replicas:
+        #     if replica not in missingContainerOut:
+        #         print(f"{replica} is failed initiate, Trying to respwn")
+        #         res=os.popen(f"sudo docker run --name {replica} --network net1 --network-alias {replica} -e 'SERVER_ID={replica}' -d server:latest").read()
+
+        #         if len(res)==0:
+        #             print(f"Unable to start {replica}")
+        #         else:
+        #             print(f"successfully started {replica}")
+
+        ################Remove Unwanted container or respwn previous container automatically #############################
+        extraContainerOut = os.popen("sudo docker ps --format '[[ .Names ]]'").read().split("\n")
+        extraContainerOut = extraContainerOut[:len(extraContainerOut)-1]
+        for container in extraContainerOut:
+            if container not in replicas and container=="loadbalancer":
+                os.system(f"sudo docker stop {container} && sudo docker rm {container}")
         time.sleep(1)
 
 ################ Calling Server thread ###############
 server_thread = threading.Thread(target=replica_status,args=(replicas,))
-
 server_thread.start()
 
 
@@ -122,8 +145,8 @@ def remove_replicas():
         else:
             replica = random.choice(replicas)
         removed_replicas.append(replica)
-        # os.system(f"sudo docker stop {replica} && sudo docker rm {replica}")
-        os.system(f"sudo docker stop {replica}")
+        os.system(f"sudo docker stop {replica} && sudo docker rm {replica}")
+        # os.system(f"sudo docker stop {replica}")
         replicas.remove(replica)
         # consistent_hash_map.remove_server_container(int(replica[1:]))
         consistent_hash_map.remove_server_container(replica)
